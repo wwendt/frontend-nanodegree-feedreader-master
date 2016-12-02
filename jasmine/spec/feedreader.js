af988ef7c208902b5859bc2de4e930289f09ceb3
Wes <westin.wendt2@gmail.com>
2016-12-02 13:34:02 -0800

    message

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -3,11 +3,22 @@
  * This is the spec file that Jasmine will read and contains
  * all of the tests that will be run against your application.
  */
+document.write('<script src="jasmine/lib/jasmine-2.1.2/Jasmine-jQuery.js"></script>');
+ 
 
 /* We're placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
+
+
+
+
  */
+
+
+
+
+
 $(function() {
     /* This is our first test suite - a test suite just contains
     * a related set of tests. This suite is all about the RSS
